9c08b9fe2453f729e4024fa4855116ebac8d5daf
rduvalwa5 <rduvalwa5@hotmail.com>
2018-11-25 22:47:21 -0800

    add tutorial and simple code

---
+++ src/simpleClient.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 25, 2018
+
+https://www.tutorialspoint.com/python/python_networking.htm
+'''
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print (s.recv(1024))
+s.close                     # Close the socket when done
