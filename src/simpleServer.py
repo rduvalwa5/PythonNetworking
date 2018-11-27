db9a3b7e4e079d7f2337437400aa4f947f07bb32
rduvalwa5 <rduvalwa5@hotmail.com>
2018-11-26 19:35:26 -0800

    simple server change

--- src/simpleServer.py
+++ src/simpleServer.py
@@ -2,14 +2,26 @@
 Created on Nov 25, 2018
 
 https://www.tutorialspoint.com/python/python_networking.htm
+
+C1246895-osx:src rduvalwa2$ python simpleServer.py 
+^CTraceback (most recent call last):
+  File "simpleServer.py", line 17, in <module>
+    c, addr = s.accept()     # Establish connection with client.
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/socket.py", line 206, in accept
+    sock, addr = self._sock.accept()
+KeyboardInterrupt
+C1246895-osx:src rduvalwa2$ python simpleServer.py 
+('Got connection from', ('192.168.1.8', 49559))
+('Got connection from', ('192.168.1.8', 49560))
+
 '''
 #!/usr/bin/python           # This is server.py file
 
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+host = "c1246895-osx2.home"# socket.gethostname() # Get local machine name 
+port = 12349                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
